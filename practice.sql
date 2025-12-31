a470d46c1e530f2cfe34f19726087a24d2e44210
Aashriya Rawat <aashriyarawat11@gmail.com>
2025-12-31 15:35:05 +0530

    Add queries-31Dec2025

--- practice.sql
+++ practice.sql
@@ -1,3 +1,4 @@
+#30-12-2025
 -- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
 SELECT DISTINCT CITY FROM STATION
 WHERE ID % 2 = 0;
@@ -42,3 +43,13 @@ WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U');
 -- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION 
 WHERE CITY NOT REGEXP '^[aeiouAEIOU]' OR CITY NOT REGEXP '[aeiouAEIOU]$';
+
+#31-12-2025
+-- Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT NAME FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME,3), ID ASC;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+SELECT NAME FROM EMPLOYEE
+ORDER BY NAME ASC;

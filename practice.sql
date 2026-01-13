fb46553d82348fe610d16aadda40b1260616cc92
Aashriya Rawat <aashriyarawat11@gmail.com>
2026-01-13 23:45:42 +0530

    Added Query - 13Jan2026
    Added SQL query to retrieve hackers with multiple full scores.

--- practice.sql
+++ practice.sql
@@ -117,4 +117,17 @@ CASE WHEN gr.Grade < 8 THEN NULL
 WHEN gr.Grade >= 8 THEN st.Name END,
 gr.Grade, st.marks FROM students st JOIN grades gr
 WHERE st.marks BETWEEN gr.min_mark AND gr.max_mark
-ORDER BY gr.grade DESC, st.name ASC
+ORDER BY gr.grade DESC, st.name ASC;
+
+-- Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
+-- Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. 
+-- If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+SELECT H.hacker_id, H.name
+FROM Hackers H
+JOIN Submissions S ON H.hacker_id = S.hacker_id
+JOIN Challenges C ON S.challenge_id = C.challenge_id
+JOIN Difficulty D ON C.difficulty_level = D.difficulty_level
+WHERE S.score = D.score
+GROUP BY H.hacker_id, H.name
+HAVING COUNT(S.challenge_id) > 1
+ORDER BY COUNT(S.challenge_id) DESC, H.hacker_id ASC;

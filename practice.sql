0c91a63017bdfb36752f650494e6156433c04aa4
Aashriya Rawat <aashriyarawat11@gmail.com>
2026-01-13 23:46:59 +0530

    Add query - 13Jan2026
    Added SQL queries for student report and coding contest leaderboard.

--- practice.sql
+++ practice.sql
@@ -109,6 +109,7 @@ FROM CITY
 INNER JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
 GROUP BY COUNTRY.Continent;
 
+#11-01-2026
 -- Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade 
 -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. 
 -- If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
@@ -119,6 +120,7 @@ gr.Grade, st.marks FROM students st JOIN grades gr
 WHERE st.marks BETWEEN gr.min_mark AND gr.max_mark
 ORDER BY gr.grade DESC, st.name ASC;
 
+#13-01-2026
 -- Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
 -- Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. 
 -- If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.

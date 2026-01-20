52f0755b3ab5e66c3583eedc2fffdc72c922d89c
Aashriya Rawat <aashriyarawat11@gmail.com>
2026-01-20 23:22:58 +0530

    Add query - 20Jan2026
    Add query to calculate rounded sums of LAT_N and LONG_W from STATION table.

--- practice.sql
+++ practice.sql
@@ -138,3 +138,10 @@ WHERE POPULATION > 100000;
 GROUP BY H.hacker_id, H.name
 HAVING COUNT(S.challenge_id) > 1
 ORDER BY COUNT(S.challenge_id) DESC, H.hacker_id ASC;
+
+#20-01-2026
+-- Query the following two values from the STATION table:
+-- The sum of all values in LAT_N rounded to a scale of  decimal places.
+-- The sum of all values in LONG_W rounded to a scale of  decimal places.
+SELECT ROUND(SUM(LAT_N),2) AS lat, ROUND(SUM(LONG_W),2) AS lon 
+FROM STATION;

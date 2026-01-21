edd97319b67687c94552ff3d08448f97ee1f3384
Aashriya Rawat <aashriyarawat11@gmail.com>
2026-01-21 23:57:29 +0530

    Add newline at end of practice.sql
    Fix SQL syntax by adding a newline at the end of the file.

--- practice.sql
+++ practice.sql
@@ -144,4 +144,4 @@ ORDER BY COUNT(S.challenge_id) DESC, H.hacker_id ASC;
 -- The sum of all values in LAT_N rounded to a scale of  decimal places.
 -- The sum of all values in LONG_W rounded to a scale of  decimal places.
 SELECT ROUND(SUM(LAT_N),2) AS lat, ROUND(SUM(LONG_W),2) AS lon 
-FROM STATION;
+FROM STATION

fd4ce05527e01e66fecd26c68785145c9821dd6f
Aashriya Rawat <aashriyarawat11@gmail.com>
2026-01-21 23:59:17 +0530

    Add query - 21Jan2026
    Added query to truncate sum of Northern Latitudes from STATION.

--- practice.sql
+++ practice.sql
@@ -144,4 +144,11 @@ ORDER BY COUNT(S.challenge_id) DESC, H.hacker_id ASC;
 -- The sum of all values in LAT_N rounded to a scale of  decimal places.
 -- The sum of all values in LONG_W rounded to a scale of  decimal places.
 SELECT ROUND(SUM(LAT_N),2) AS lat, ROUND(SUM(LONG_W),2) AS lon 
-FROM STATION
+FROM STATION;
+
+#21-01-2026
+-- Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880  and less than 137.2345 Truncate your answer to  decimal places.
+SELECT TRUNCATE(SUM(LAT_N), 4) FROM STATION 
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345 ;
+
+

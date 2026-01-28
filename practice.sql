353e6798f8e93c2c34440e074eb52d93424ee41b
Aashriya Rawat <aashriyarawat11@gmail.com>
2026-01-28 22:44:36 +0530

    Add query - 28Jan2026

--- practice.sql
+++ practice.sql
@@ -157,3 +157,10 @@ SELECT ROUND(LONG_W,4) FROM STATION
 WHERE LAT_N < 137.2345
 ORDER BY LAT_N DESC
 LIMIT 1;
+
+#28-01-2026
+-- Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780. Round your answer to 4 decimal places.
+SELECT ROUND(LONG_W,4) FROM STATION
+WHERE LAT_N > 38.7780 
+ORDER BY LAT_N ASC
+LIMIT 1;

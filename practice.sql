0776abba4b7d3b3f279a16417d1d7b5a3a2ab21d
Aashriya Rawat <aashriyarawat11@gmail.com>
2026-02-18 23:45:33 +0530

    Add query - 18Feb2026

--- practice.sql
+++ practice.sql
@@ -245,3 +245,9 @@ SELECT Name
 FROM STUDENTS 
 WHERE Marks > 75
 ORDER BY RIGHT(Name,3), ID ASC;
+
+#18-02-2026
+-- Consider P1(a,c) and P2(b,d) to be two points on a 2D plane where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+-- Query the Euclidean Distance between points P1 and P2 and format your answer to display  decimal digits.
+SELECT ROUND(SQRT(POWER(MAX(LAT_N) - MIN(LAT_N),2) + POWER(MAX(LONG_W) - MIN(LONG_W),2)),4)
+FROM STATION;

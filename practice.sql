6bf1178115d2e326a5b158338466f009c64ea7dd
Aashriya Rawat <aashriyarawat11@gmail.com>
2026-02-20 23:58:38 +0530

    Add query - 20Feb2026
    Add query to select non-evil wands sorted by power and age.

--- practice.sql
+++ practice.sql
@@ -251,3 +251,24 @@ ORDER BY RIGHT(Name,3), ID ASC;
 -- Query the Euclidean Distance between points P1 and P2 and format your answer to display  decimal digits.
 SELECT ROUND(SQRT(POWER(MAX(LAT_N) - MIN(LAT_N),2) + POWER(MAX(LONG_W) - MIN(LONG_W),2)),4)
 FROM STATION;
+
+#20-02-2026
+-- Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
+-- Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to buy each non-evil wand of high power and age. Write a query to print the id, age, coins_needed, and power of the wands 
+-- that Ron's interested in, sorted in order of descending power. If more than one wand has same power, sort the result in order of descending age.
+SELECT 
+    w.id, 
+    wp.age, 
+    w.coins_needed, 
+    w.power 
+FROM Wands AS w
+JOIN Wands_Property AS wp ON w.code = wp.code 
+WHERE wp.is_evil = 0 
+AND w.coins_needed = (
+    SELECT MIN(w1.coins_needed) 
+    FROM Wands AS w1 
+    JOIN Wands_Property AS wp1 ON w1.code = wp1.code 
+    WHERE wp1.age = wp.age 
+    AND w1.power = w.power
+) 
+ORDER BY w.power DESC, wp.age DESC;

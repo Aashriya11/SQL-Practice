49f7a5942af9877c8c3d5b31c1b36ae734bfe681
Aashriya Rawat <aashriyarawat11@gmail.com>
2026-02-26 18:14:18 +0530

    Add query - 26Feb2026

--- practice.sql
+++ practice.sql
@@ -272,3 +272,18 @@ AND w.coins_needed = (
     AND w1.power = w.power
 ) 
 ORDER BY w.power DESC, wp.age DESC;
+
+#26-02-2026
+-- Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output should consist of four columns (Doctor, Professor, Singer, and Actor) in that specific order, with their respective names listed alphabetically under each column.
+-- Note: Print NULL when there are no more names corresponding to an occupation.
+SELECT MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+       MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+       MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
+       MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
+FROM (
+      SELECT Name,
+             Occupation,
+             ROW_NUMBER() OVER(PARTITION BY Occupation ORDER BY Name)as rn
+      FROM OCCUPATIONS
+     ) AS subquery
+GROUP BY rn;
